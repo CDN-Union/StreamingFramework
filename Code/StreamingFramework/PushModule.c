d50a979ea937b864bf939a9eed5d79393b5f7eac
李雪岩 <lixueyan@xunlei.com>
2017-07-20 10:30:25 +0800

    修改协议的优先级

--- Code/StreamingFramework/PushModule.c
+++ Code/StreamingFramework/PushModule.c
@@ -21,9 +21,9 @@ extern push_module_t rtmppush_module;
 extern push_module_t wspush_module;
 /* 定义所有模块，优先级高的在前 */
 push_module_t *global_modules[] = {
-    &rtmppush_module,
-    &xypush_module,
+    &xypus∂h_module,
     &wspush_module,
+    &rtmppush_module,
     /* 其他厂商的模块加在这里即可 */
 };
 
